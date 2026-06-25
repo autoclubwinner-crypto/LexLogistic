b6147d75b79ee94fdee0dfe5876faf1bbf3a2b44
autoclubwinner-crypto <autoclubwinner@gmail.com>
2026-06-25 12:56:26 +0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -97,7 +97,7 @@ export default function App() {
 
       // Calculations according to business rules:
       const rubUsdt = usdtRubRaw;
-      const rubUsdTbilisi = usdtRubRaw * 1.014;
+      const rubUsdTbilisi = usdtRubRaw * 1.013;
       const rubUsdSwift = usdtRubRaw * 1.01;
       
       // Cross rate XE EUR to USD
