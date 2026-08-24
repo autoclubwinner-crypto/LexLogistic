b8dde28a7c086c121882f8c1a0346d53f83563cc
autoclubwinner-crypto <autoclubwinner@gmail.com>
2026-08-24 16:03:08 +0400

    Delete test-cache.js

--- test-cache.js
+++
@@ -1,7 +0,0 @@
-const { updateCache, getCachedRates } = require('./dist/server.cjs');
-(async () => {
-  console.log("Fetching...");
-  await updateCache();
-  const rates = await getCachedRates();
-  console.log("Result:", rates);
-})();
